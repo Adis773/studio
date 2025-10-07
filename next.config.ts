a953959c06d3375cd3beacfbe4105c1c6e958c14
Adis Abenov <adis040512@gmail.com>
2025-10-07 16:18:06 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -29,12 +29,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'cdn.freekassa.ru',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
   allowedDevOrigins: [
