07baf5e630683fbb9edc39f72ca1b2e23dda9c84
Adis Abenov <adis040512@gmail.com>
2025-10-07 16:45:47 +0000

    e selected A Node.js API is used (process.versions at line: 35) which is

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@
 import { type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/middleware'
 
+export const runtime = 'nodejs'
+
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
 }
