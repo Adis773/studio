d025001cf83bfaca522f230c3b1ad90eb41cb820
Adis Abenov <adis040512@gmail.com>
2025-10-08 08:11:04 +0000

    Нет я хочу чтобы ключи хранились в самом коде

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,13 +1,9 @@
 
 import { createBrowserClient } from '@supabase/ssr'
 
-// IMPORTANT: The environment variables are set in the .env file.
-// NEXT_PUBLIC_SUPABASE_URL & NEXT_PUBLIC_SUPABASE_ANON_KEY
-// They are not available in this file, but are in the browser.
-
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    "https://pxbogsidwrkdcdboohdr.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4Ym9nc2lkd3JrZGNkYm9vaGRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyMDQ0OTMsImV4cCI6MjA0MDc4MDQ5M30.s433Tj5a-A5B20K-v-3m2i-9qV6Qc49D8e1fA6b4c2c"
   )
 }
